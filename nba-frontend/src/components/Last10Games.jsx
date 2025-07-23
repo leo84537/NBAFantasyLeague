4faa592d26cdbee0e796013840f475e877f68008
Leo Sun <leosun84537@gmail.com>
2025-07-23 14:52:31 -0700

    API url changed to render

--- nba-frontend/src/components/Last10Games.jsx
+++ nba-frontend/src/components/Last10Games.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import api from "../main";
+const API_URL = import.meta.env.VITE_API_URL;
+
 
 export default function Last10Games({ playerId }) {
   const [games, setGames] = useState([]);
--- nba-frontend/src/main.jsx
+++ nba-frontend/src/main.jsx
@@ -14,9 +14,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   </React.StrictMode>
 );
 
-
+// baseURL "http://localhost:8000"
 const api = axios.create({
-  baseURL: "http://localhost:8000", 
+  baseURL: import.meta.env.VITE_API_URL, 
 });
 
 export default api;
